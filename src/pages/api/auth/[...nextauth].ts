482fded3f556682da645ca8ab6cb963d5179a73e
Gabriel Soares <gabrielgyns@gmail.com>
2021-10-02 14:36:37 -0300

    Saving user on faunaDB when signIn

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +11,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
       scope: 'read:user'
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                ) // q.Match
+              ) // q.Exists
+            ), // q.Not
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ), // q.Create
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              ) // q.Match
+            ) // q.Get
+          ) // q.If
+        );
+        
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 });
