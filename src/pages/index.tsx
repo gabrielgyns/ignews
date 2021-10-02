f453ac4d1ca10af630ddc8b750024c24545bb5ab
Gabriel Soares <gabrielgyns@gmail.com>
2021-10-02 01:38:21 -0300

    Integration with Stripe API and applying Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { formatMoney } from '../utils';
+import { stripe } from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from '../styles/home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+const TWENTY_FOUR_HOURS_IN_SECONDS = 60 * 60 * 24; // 86400 seconds, 24 hours;
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head><title>Home | ig.news</title></Head>
@@ -14,14 +26,31 @@ export default function Home() {
           <h1>News about <br /> the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
           
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Ih1mQFqwl6ta6aVByUzTPjf');
+
+  const product = {
+    priceId: price.id,
+    amount: formatMoney(price.unit_amount / 100),
+
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: TWENTY_FOUR_HOURS_IN_SECONDS
+  }
 }
