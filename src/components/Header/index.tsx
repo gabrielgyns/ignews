d042f9976b8440d717f26bddfaf8253e8321a589
Gabriel Soares <gabrielgyns@gmail.com>
2021-10-03 13:12:59 -0300

    Implementing posts dynamic with Prismic and Navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,24 @@
-// import Image from 'next/image'
+// import Image from 'next/image';
+import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
 
-import { SignInButton } from '../SignInButton'
-
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
 export function Header() {
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 
                 <nav>
-                    <a href="" className={styles.active}>Home</a>
-                    <a href="">Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
